2a7f98718b3686d94fd37cc9df6b957984812f7c
abdoulkader <zarikader@gmail.com>
2024-02-15 22:18:36 +0100

    MA

--- assets/scripts.js
+++ assets/scripts.js
@@ -14,23 +14,3 @@ $(document).ready(function () {
   });
 });
 
-
-const contact = document.querySelector(".contact");
-
-let displayOne = true;
-window.addEventListener("scroll", () => {
-  console.log(window.scrollY);
-  if (window.scrollY > 3709 && displayOne) {
-    contact.style.opacity = 1;
-    contact.style.transform = "none";
-    displayOne = false;
-  }
- 
-});
-const closeBtn = document.querySelector("#closeBtn");
-closeBtn.addEventListener("click", () => {
-  contact.style.opacity = "0";
-  contact.style.transform = "translate(700px)";
-  displayOne = false;
-  console.log("test");
-});
