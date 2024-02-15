e7ffc0c1b5c9d02d036cae921c69dfd60a102299
abdoulkader <zarikader@gmail.com>
2024-02-15 21:32:33 +0100

    upd

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,36 +1,36 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+$(document).ready(function () {
+  $(".gallery").mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  });
 });
-// let scrollValue = (window.scrollY + window.innerHeight) / document.body.offsetHeight;
 
-const contact = document.querySelector('.contact');
+
+const contact = document.querySelector(".contact");
 
 let displayOne = true;
-window.addEventListener('scroll', () => { 
-console.log(window.scrollY);
-if( window.scrollY > 3709 && displayOne) {
+window.addEventListener("scroll", () => {
+  console.log(window.scrollY);
+  if (window.scrollY > 3709 && displayOne) {
     contact.style.opacity = 1;
     contact.style.transform = "none";
     displayOne = false;
-}
-
-})
-const closeBtn = document.querySelector('#closeBtn');
-closeBtn.addEventListener('click', () => {
-    contact.style.opacity = "0";
-    contact.style.transform = "translate(700px)";
-    displayOne=false;
-    console.log("test");
-})
+  }
+ 
+});
+const closeBtn = document.querySelector("#closeBtn");
+closeBtn.addEventListener("click", () => {
+  contact.style.opacity = "0";
+  contact.style.transform = "translate(700px)";
+  displayOne = false;
+  console.log("test");
+});
